01b8f07a8c9a2a2cf0cd7eaf5e150fbacf28c70b
GBICTS LIMITED <GBICTS LIMITED@GBICTS-LIMITED>
2020-07-03 14:00:23 +0100

    running 'yarn build' to create dist directory

---
+++ dist/precache-manifest.a3c177f36dc7ca49aaf35a7a579d3982.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df64a5d3035fc62c5b7d",
+    "url": "/css/app.752a0c5a.css"
+  },
+  {
+    "revision": "1ff9e01fd256bc584bd9",
+    "url": "/css/chunk-vendors.9b9fad60.css"
+  },
+  {
+    "url": "/img/8782463765d8f7b6c77b07bed82c30aa.87824637.png"
+  },
+  {
+    "url": "/img/f7cbf61bd9900dd76b80e222c65cf8be.f7cbf61b.png"
+  },
+  {
+    "revision": "824e22b67904030fe670feb1d3d2a75e",
+    "url": "/img/undraw_bear_market_ania.824e22b6.png"
+  },
+  {
+    "revision": "8276c8a923d6167fb5b412774519c3c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df64a5d3035fc62c5b7d",
+    "url": "/js/app.89286d40.js"
+  },
+  {
+    "revision": "1ff9e01fd256bc584bd9",
+    "url": "/js/chunk-vendors.f42bb71e.js"
+  },
+  {
+    "revision": "aea1ccd32550332e108747bffc13aed7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
