589b18297a1eae34677ec848cb2171ccaccf1b1c
GBICTS LIMITED <GBICTS LIMITED@GBICTS-LIMITED>
2020-07-03 20:45:10 +0100

    debugging favicon issue

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const app = require("express")();
+const favicon = require("express-favicon");
+
 const serverStatic = require("serve-static");
 const path = require("path");
-
+app.use(favicon(path.join(__dirname, "./public/favicon.ico")));
 app.use("/", serverStatic(path.join(__dirname, "/dist")));
 
 const port = process.env.PORT;
