327b32803ad428428b5c99a59a91f9cba49a9d88
GBICTS LIMITED <GBICTS LIMITED@GBICTS-LIMITED>
2020-07-03 14:50:48 +0100

    removal irrelevants

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const serverStatic = require("serve-static");
 const path = require("path");
 
 app.use("/", serverStatic(path.join(__dirname, "/dist")));
-app.get(/.*/, (req, res) => {
-  res.sendfile(path.join(__dirname, "/dist/index.html"));
-});
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`app is listening on port ${port}`));
